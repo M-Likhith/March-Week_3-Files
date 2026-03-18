303d2f075215285a7a03c8bc0ac2eeab3796242d
M-Likhith <likhith.mentrumilli@gmail.com>
2026-03-18 12:50:03 +0530

    Update min,max,sum & Student marks task.py

--- min,max,sum & Student marks task.py
+++ min,max,sum & Student marks task.py
@@ -7,18 +7,19 @@ print(sum(a))
 print(sum([3,5,6,7,8,9,10,20,30]))'''
 
 #student marks analysis
-while True:
-    a=int(input("enter no of stdents:"))
-    marks=[]
-    for i in range (a):
-            b=int(input("enter the marks {i+1}:"))
-        high=max(marks)
+stud=int(input("enter no of stdents:"))
+marks=[]
+for i in range (stud):
+    a=int(input("enter the marks :"))
+    marks.append(a)
+for j in marks:
+    print(j)
+high=max(marks)    
 low=min(marks)
-average=sum(marks)/n
+total=sum(marks)
+average=total//stud
 print(" Results  ")
 print("highest marks:",high)
 print("lowest marks:",low)
+print("total marks:",total)
 print("average marks:",average)
-        
-    
-    
